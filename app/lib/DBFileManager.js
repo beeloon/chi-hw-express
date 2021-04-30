2fe98e77e972240826be1ac7707fd96b386569da
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2021-04-30 12:41:53 +0300

    Rework DBFileManager.js file

--- app/lib/DBFileManager.js
+++ app/lib/DBFileManager.js
@@ -7,6 +7,35 @@ export default class DBFileManager {
     this.file = file;
   }
 
+  static async init(path, ...entities) {
+    await this.initializeDirectory(path);
+    const fileNames = entities.map((entity) => entity.DBName);
+
+    for (let file of fileNames) {
+      await this.initializeFile(path, file);
+    }
+  }
+
+  static async initializeFile(path, file) {
+    const filePath = resolve(path, `${file}.json`);
+
+    try {
+      await access(filePath);
+    } catch (err) {
+      await writeFile(filePath, "[]");
+    }
+  }
+
+  static async initializeDirectory(path) {
+    const pathToDir = resolve(path);
+
+    try {
+      await access(pathToDir);
+    } catch (err) {
+      await mkdir(pathToDir);
+    }
+  }
+
   async addEntityToFile(entityData) {
     const fileContent = await this.getFileContent(this.file);
 
@@ -39,17 +68,15 @@ export default class DBFileManager {
   async updateEntityById(id, newData) {
     const fileContent = await this.getFileContent(this.file);
 
-    const newContent = fileContent.map((entity) => {
-      if (entity.id === id) {
-        return {
-          id,
-          ...entity,
-          ...newData,
-        };
-      }
-
-      return entity;
-    });
+    const newContent = fileContent.map((entity) =>
+      entity.id === id
+        ? {
+            id,
+            ...entity,
+            ...newData,
+          }
+        : entity
+    );
 
     await this.setFileContent(this.file, JSON.stringify(newContent));
 
@@ -99,35 +126,6 @@ export default class DBFileManager {
       return content;
     } catch (err) {
       throw new Error(err);
-    }
-  }
-
-  static async init(path, ...entities) {
-    await this.initializeDirectory(path);
-    const fileNames = entities.map((entity) => entity.DBName);
-
-    for (let file of fileNames) {
-      await this.initializeFile(path, file);
-    }
-  }
-
-  static async initializeFile(path, file) {
-    const filePath = resolve(path, `${file}.json`);
-
-    try {
-      await access(filePath);
-    } catch (err) {
-      await writeFile(filePath, "[]");
-    }
-  }
-
-  static async initializeDirectory(path) {
-    const pathToDir = resolve(path);
-
-    try {
-      await access(pathToDir);
-    } catch (err) {
-      await mkdir(pathToDir);
     }
   }
 }
