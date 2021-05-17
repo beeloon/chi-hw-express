05402bf3df85176a110c913bb584ed7cd2e8edd5
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2021-05-17 15:32:13 +0300

    add(routes): add follower foutes to router

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,14 @@ import express from 'express';
 
 import { createUserRoutes } from './modules/user/user.routes';
 import { createPostRoutes } from './modules/post/post.routes';
+import { createFollowerRoutes } from './modules/follower/follower.routes';
 
 export const createRouter = (app) => {
   const router = express.Router();
 
-  [createUserRoutes, createPostRoutes].forEach((fn) => fn(router));
+  createUserRoutes(router);
+  createPostRoutes(router);
+  createFollowerRoutes(router);
 
   app.use('/api', router);
 };
