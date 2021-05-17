e9a99ee67f8d713177c16679873c4e6346b6cc96
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2021-05-17 14:47:45 +0300

    rework(dependensies/config)install sequelize and customize sequelizerc with config folder

---
+++ app/config/default.cjs
@@ -0,0 +1,12 @@
+const config = {
+  development: {
+    username: 'root',
+    password: '1q2w3e!Q',
+    database: 'express_api_server',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    PORT: 8000,
+  },
+};
+
+module.exports = config;
--- config/default.cjs
+++
@@ -1,6 +0,0 @@
-const config = {
-  pathToDBFolder: process.env.DB_DATA_FOLDER_PATH,
-  serverPort: process.env.PORT,
-};
-
-module.exports = config;
