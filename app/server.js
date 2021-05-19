8ebc916f30d59e5c8c7f221ec4ca051854882122
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2021-05-19 16:36:18 +0300

    delete(app): delete middlewares folder and imported functions from server.js

--- app/server.js
+++ app/server.js
@@ -2,17 +2,13 @@ import express from 'express';
 
 import database from './database';
 import { createRouter } from './routes';
-import { requestLogger, errorHandler } from './middlewares';
 
 const PORT = process.env.PORT;
 const app = express();
 
-app.use(requestLogger);
 app.use(express.json());
 
 createRouter(app);
-
-app.use(errorHandler);
 
 app.listen(PORT, async () => {
   await database.sequelize.authenticate();
